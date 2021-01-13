1c0ec9c6eff523092026b085f081ff86ea493d86
p.thibault10 <thibault@spliiit.com>
2021-01-13 14:55:42 +0100

    1rst setup with useless demo

---
+++ Formation/assets/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <div class="container">
+        <h1>Ceci est notre composant de démo !</h1>
+        <button @click="counter++">Click me ! {{counter}}</button>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+      counter : 0
+  })
+};
+</script> 

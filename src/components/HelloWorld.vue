53b00ceda95a47f9872ffbdabb2ecd60ee774ea9
Adam Sellin <marcsellin2016@gmail.com>
2021-01-21 23:41:58 +0100

    add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="hello">
+  <div id="app" >
     <h1>Formations</h1>
     <table>
+      <thead>
       <tr>
         <th scope="col">ID</th>
         <th scope="col">Nom</th>
@@ -11,33 +12,62 @@
         <th scope="col">Niveau</th>
         <th scope="col">Durée</th>
       </tr>
-      <tr>
-        <td>1</td>
-        <td>Paris</td>
-        <td>75001</td>
-        <td>15/01/2021</td>
-        <td>LP DW</td>
-        <td>Bac + 3</td>
-        <td>12</td>
+      </thead>
+      <tbody>
+      <tr v-for="(currency, index) in filteredFormation" :key="index">
+        <td>{{ currency.id }}</td>
+        <td>{{ currency.typeFormation}}</td>
+        <td>{{ currency.Date}}</td>
+        <td>{{ currency.Lieux }}</td>
+        <td>{{ currency.Departement }}</td>
+        <td>{{ currency.niveauFormation}}</td>
+        <td>{{ currency.duréeFormation}}</td>
       </tr>
-      <tr>
-        <td>2</td>
-        <td>Paris</td>
-        <td>75001</td>
-        <td>13/01/2021</td>
-        <td>LP WAM</td>
-        <td>Bac + 3</td>
-        <td>12</td>
-      </tr>
+      </tbody>
     </table>
+    <input type="text"
+           class="SearchBar"
+           placeholder="Filter by Formations"
+           v-model="nameFormationSearchString"
+    />
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     name: 'HelloWorld',
-    props: {
-      msg: String
+    el: '#app',
+    data () {
+      return {
+        nameFormationSearchString:'',
+        sortKey: '',
+        info: null
+      }
+    },
+    mounted () {
+      axios
+              .get('http://localhost:8000/api/formations')
+              .then(response => (this.info = response.data['hydra:member']))
+    },
+    computed: {
+      filteredFormation: function() {
+
+        var formation = this.info;
+        var nameFormationSearchString = this.nameFormationSearchString;
+
+        if(!nameFormationSearchString){
+            return formation;
+        }
+
+        formation = formation.filter(function (item) {
+          if(item.typeFormation.toLowerCase().indexOf(nameFormationSearchString) !== -1){
+            return item;
+          }
+        })
+        return formation;
+      }
     }
   }
 </script>
@@ -49,5 +79,10 @@
   }
   table {
     width: 100%;
+  }
+  .SearchBar {
+    margin-top: 50px;
+    width: 20%;
+    padding: 10px;
   }
 </style>

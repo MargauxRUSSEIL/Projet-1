28fe1f5cb07e5ce493139d46379a3c5f0de5321a
Adam Sellin <marcsellin2016@gmail.com>
2021-02-07 19:13:24 +0100

    filtre/tri des données du tableau, suppression

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,13 +4,14 @@
     <table>
       <thead>
       <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Nom</th>
-        <th scope="col">Date</th>
-        <th scope="col">Lieux</th>
-        <th scope="col">Département</th>
-        <th scope="col">Niveau</th>
-        <th scope="col">Durée</th>
+        <th v-on:click="sort('id')" scope="col">ID</th>
+        <th v-on:click="sort('typeFormation')" scope="col">Nom</th>
+        <th v-on:click="sort('Date')" scope="col">Date</th>
+        <th v-on:click="sort('Lieux')" scope="col">Lieux</th>
+        <th v-on:click="sort('Departement')" scope="col">Département</th>
+        <th v-on:click="sort('niveauFormation')" scope="col">Niveau</th>
+        <th v-on:click="sort('duréeFormation')" scope="col">Durée</th>
+        <th scope="col">Actions</th>
       </tr>
       </thead>
       <tbody>
@@ -22,6 +23,7 @@
         <td>{{ currency.Departement }}</td>
         <td>{{ currency.niveauFormation}}</td>
         <td>{{ currency.duréeFormation}}</td>
+        <td><button v-on:click="deleteFormation(currency.id)">Supprimer</button></td>
       </tr>
       </tbody>
     </table>
@@ -43,13 +45,34 @@
       return {
         nameFormationSearchString:'',
         sortKey: '',
-        info: null
+        info: [],
+        currentSort:'id',
+        currentSortDir:'asc'
       }
     },
     mounted () {
-      axios
-              .get('http://localhost:8000/api/formations')
-              .then(response => (this.info = response.data['hydra:member']))
+      this.getFormation()
+    },
+    methods: {
+      getFormation: function () {
+        axios
+                .get('http://localhost:8000/api/formations')
+                .then(response => (this.info = response.data['hydra:member']))
+      },
+      deleteFormation: function (id) {
+        axios
+                .delete('http://localhost:8000/api/formations/' + id)
+                .then(() => {
+                  this.getFormation();
+                })
+      },
+      sort:function(s) {
+        //if s == current sort, reverse
+        if(s === this.currentSort) {
+          this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+        }
+        this.currentSort = s;
+      }
     },
     computed: {
       filteredFormation: function() {
@@ -57,17 +80,30 @@
         var formation = this.info;
         var nameFormationSearchString = this.nameFormationSearchString;
 
+        if (formation) {
+          // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+          return this.info.sort((a,b) => {
+            let modifier = 1;
+            if(this.currentSortDir === 'desc') modifier = -1;
+            if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+            if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+            return 0;
+          });
+        }
+
         if(!nameFormationSearchString){
-            return formation;
+          return formation;
         }
 
         formation = formation.filter(function (item) {
-          if(item.typeFormation.toLowerCase().indexOf(nameFormationSearchString) !== -1){
+          if(item.typeFormation.toLowerCase().indexOf(nameFormationSearchString) !== -1 || item.typeFormation.toUpperCase().indexOf(nameFormationSearchString) !== -1 ){
             return item;
           }
         })
         return formation;
-      }
+
+
+      },
     }
   }
 </script>

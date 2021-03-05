29f94ba78cb268fdae623b755dc1df3e324ea6d5
Adam Sellin <marcsellin2016@gmail.com>
2021-03-05 02:41:49 +0100

    add

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="app">
-    <HelloWorld/>
-    <Table/>
+    <v-app>
+      <v-tabs>
+        <v-tab>POC</v-tab>
+        <v-tab>Table Vuetify</v-tab>
+        <!--<v-tab>Table Vue JS Simple</v-tab>-->
+        <v-tab-item>
+          <POC/>
+        </v-tab-item>
+        <v-tab-item>
+          <Table/>
+        </v-tab-item>
+        <!--<v-tab-item>
+          <HelloWorld/>
+        </v-tab-item>-->
+      </v-tabs>
+    </v-app>
+
+
+
   </div>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue'
+  //import HelloWorld from './components/HelloWorld.vue'
   import Table from "@/components/Table"
+  import POC from "@/components/POC";
 
   export default {
     name: 'App',
     components: {
-      HelloWorld,
+      POC,
+      //HelloWorld,
       Table
     }
   }
--- src/components/POC.vue
+++ src/components/POC.vue
@@ -1,10 +1,51 @@
 <template>
-    $END$
+    <v-app>
+        <div v-for="(currency, index) in datafacker" :key="index">
+            <template v-if="currency.type_field === 'inputText'">
+                <v-flex xs5>
+                    <v-text-field
+                            :label="currency.label_field"
+                            outlined
+                    ></v-text-field>
+                </v-flex>
+            </template>
+            <template v-else-if="currency.type_field === 'inputNumber'">
+                <v-flex xs5>
+                    <v-text-field
+                            :label="currency.label_field"
+                            type="number"
+                            outlined
+                    ></v-text-field>
+                </v-flex>
+            </template>
+            <template v-else-if="currency.type_field === 'select'">
+                    <v-flex xs5>
+                        <v-select
+                                :items="options"
+                                :label="currency.label_field"
+                                outlined
+                                chips
+                                deletable-chips
+                        ></v-select>
+                    </v-flex>
+            </template>
+        </div>
+    </v-app>
 </template>
 
 <script>
     export default {
-        name: "POC"
+        name: "POC",
+        data() {
+            return {
+                datafacker: [
+                    { label_field:"Nom du diplôme", type_field:"inputText", value_field:"string" },
+                    { label_field:"Nombre ECCTS", type_field:"inputNumber", value_field:"number" },
+                    { label_field:"Composante", type_field:"select", value_field:"string"},
+                ],
+                options: [ "science", "droit" ]
+            }
+        }
     }
 </script>
 

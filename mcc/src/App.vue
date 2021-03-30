9480ffbf19ac42beb9696a62e57da80478d68a77
salavudeenH <salavudeenhadji@gmail.com>
2021-03-30 13:28:20 +0200

    first commit

---
+++ mcc/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  
+  <Header/>
+  <Formulaire/>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Formulaire from '@/components/Formulaire.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Formulaire
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>

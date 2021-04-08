da3c77c4436ea515fefc1719814a3dd348b3dd1c
victorgbn <49316233+victorgbn@users.noreply.github.com>
2021-04-08 12:40:33 +0200

    add Tailwind

--- workflow/src/App.vue
+++ workflow/src/App.vue
@@ -1,17 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container w-auto">
+        <div class="my-12 md:mx-6 sm:mx-6 xl:mx-56 lg:mx-56">
+            <div class="flex flex-wrap ">
+                <div class="grid grid-cols-6 w-full gap-2">
+                    <div class="col-start-1 col-end-3 ...">
+                        <div class="w-full px-3">
+                            <router-link :to="{ name: 'newCompetence' }">
+                                <button class="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded font-semibold text-sm" type="button">Nouveau</button>
+                            </router-link>
+                        </div>
+                    </div>
+                    <div class="col-end-7 col-span-2 ...">
+                        <div class="w-full px-3 mb-6">
+                            <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
+                                   type="search"
+                                   placeholder="Rechercher"
+                            >
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table class="w-full table-auto divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Bloc</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Seuil Bloc</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ECTS</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Coefficient</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Seuil</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                    <tr v-for="item in competence" :key="item">
+                        <td class="px-6 py-4 whitespace-nowrap">{{ item.blocs }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ item.seuilBlocs }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-100 text-gray-800">
+                            {{ item.ECT }}
+                        </span>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-100 text-gray-800">
+                            {{ item.COEFF }}
+                        </span>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ item.seuil}}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                            <div class="text-sm text-gray-900">
+                                <router-link :to="{ name: 'updateCompetence', params: { id: item.id }}">
+                                    <button class="text-indigo-600 hover:text-indigo-900 font-semibold">Modifier</button>
+                                </router-link>
+                            </div>
+                            <div class="text-sm text-gray-900">
+                                <button class="text-indigo-600 hover:text-indigo-900 font-semibold" v-on:click="deleteCompetence(item.id)">Supprimer</button>
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>

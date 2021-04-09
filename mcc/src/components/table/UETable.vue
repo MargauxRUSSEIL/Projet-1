bd2ebc3fe53e31a66dd96ea66879e7ca7bb52479
accessiproject <kevin.bustamante@maiil.novancia.fr>
2021-04-09 03:26:58 +0200

    modifs UE

--- mcc/src/components/table/UETable.vue
+++ mcc/src/components/table/UETable.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="container w-auto">
         <div class="my-12 md:mx-6 sm:mx-6 xl:mx-56 lg:mx-56">
+        <h1>Liste des unités d'enseignement</h1>
             <div class="flex flex-wrap ">
                 <div class="grid grid-cols-6 w-full gap-2">
                     <div class="col-start-1 col-end-3 ...">
                         <div class="w-full px-3">
                             <router-link :to="{ name: 'newUE' }">
-                                <button class="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded font-semibold text-sm" type="button">Nouveau</button>
+                                <span class="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded font-semibold text-sm" type="button">Ajouter</span>
                             </router-link>
                         </div>
                     </div>
@@ -25,17 +26,17 @@
                 <table class="w-full table-auto divide-y divide-gray-200">
                     <thead class="bg-gray-50">
                     <tr>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Libellé UE</th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Unité d'enseignement</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
                     </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200">
                     <tr class="hover:bg-gray-100" v-for="item in filtered" :key="item">
-                        <td class="px-6 py-4 whitespace-nowrap">{{ item.libelleUE }}</td>
+                        <th scope="row" class="px-6 py-4 whitespace-nowrap">{{ item.libelleUE }}</th>
                         <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                             <div class="text-sm text-gray-900">
                                 <router-link :to="{ name: 'updateUE', params: { id: item.id }}">
-                                    <button class="text-indigo-600 hover:text-indigo-900 font-semibold">Modifier</button>
+                                    <span class="text-indigo-600 hover:text-indigo-900 font-semibold">Modifier</span>
                                 </router-link>
                             </div>
                             <div class="text-sm text-gray-900">

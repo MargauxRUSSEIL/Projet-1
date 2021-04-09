7ff539eac3e181e53ab817c478a842d8c56010e9
salavudeenH <salavudeenhadji@gmail.com>
2021-04-09 13:16:10 +0200

    update fix

--- vue-js/mcc/src/components/update/formationUpdate.vue
+++ vue-js/mcc/src/components/update/formationUpdate.vue
@@ -1,17 +1,78 @@
-<template>
+<template >
     <form class="container w-auto">
         <div class="my-12 md:mx-6 sm:mx-6 xl:mx-56 lg:mx-56">
             <div class="flex flex-wrap" >
                 <div class="w-full px-3 mb-6 md:mb-4">
-                     <li >
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                         structure Haute
                     </label>
                     <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
                            type="text"
-                           v-model.number="form.structureHaute" v-for="form in info" :key="form"
+                           v-model.number="form.structureHaute"
                     >
-                     </li>
+                </div>
+                <div class="w-full px-3 mb-6 md:mb-4">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Code Diplome :
+                    </label>
+                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
+                           type="text"
+                           v-model.number="form.codeDiplome"
+                    >
+                </div>
+                <div class="w-full px-3 mb-6 md:mb-4">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Ecole Doctorale :
+                    </label>
+                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
+                           type="text"
+                           v-model.number="form.ecoleDoctorale"
+                    >
+                </div>
+                 <div class="w-full px-3 mb-6 md:mb-4">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Contact :
+                    </label>
+                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
+                           type="text"
+                           v-model.number="form.contact"
+                    >
+                </div>
+                 <div class="w-full px-3 mb-6 md:mb-4">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Commentaire :
+                    </label>
+                    <textarea class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
+                           type="text"
+                           v-model.number="form.commentaire"
+                    />
+                </div>
+                <div class="w-full px-3 mb-6 md:mb-4">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Créer le :
+                    </label>
+                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
+                           type="text"
+                           v-model.number="form.createdAt"
+                    >
+                </div>
+                <div class="w-full px-3 mb-6 md:mb-4">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        VDI :
+                    </label>
+                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
+                           type="text"
+                           v-model.number="form.VDI"
+                    >
+                </div>
+                <div class="w-full px-3 mb-6 md:mb-4">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                        Statuts :
+                    </label>
+                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
+                           type="text"
+                           v-model.number="form.statuts"
+                    >
                 </div>
                 <div class="w-full px-3 mt-12">
                     <button class="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded font-semibold text-sm" type="button" v-on:click="submit()">ENVOYER</button>
@@ -30,25 +91,34 @@
         name: "FormationUpdate",
         data() {
             return {
-                info:null,
                 form: {
-                    structureHaute: ''
+                    structureHaute: '',
+                    codeDiplome:'',
+                    ecoleDoctorale:'',
+                    contact:'',
+                    commentaire:'',
+                    VDI:'',
+                    statuts:'',
+
                 }
             }
         },
         mounted(){
             var self = this;
             //Lire données Api
-            axios
-                .get('http://localhost:8000/api/formations.json')
+            let id = this.$route.params.id;
+              axios
+                .get('http://localhost:8000/api/formations/' + id)
                 .then(function (response) {
-                    self.info = response.data
+                    self.form = response.data
+                     console.log(response.data)
                 })
         },
         methods: {
             submit: function (id) {
                 id = this.$route.params.id;
-
+               
+                
                 axios.put( BaseUrl + 'formations/' + id, this.form)
                     // eslint-disable-next-line no-unused-vars
                     .then(function( response ){

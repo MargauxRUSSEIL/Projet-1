a4654c3db1f4077c2b29426ae6a1ead09bf82677
Adam Sellin <marcsellin2016@gmail.com>
2021-04-29 18:09:24 +0200

    add

--- mcc/src/App.vue
+++ mcc/src/App.vue
@@ -1,39 +1,110 @@
 <template>
-  <div id="nav">
-    <router-link to="/">MCC</router-link> |
-    <router-link to="/formation">Formation </router-link> |
-    <router-link to="/parcours">Parcours</router-link> |
-    <router-link to="/competence">Competence</router-link> |
-    <router-link :to="{name: 'Diplome'}">Type Diplome</router-link> |
-    <router-link to="/domaine">Domaine</router-link> |
-    <router-link to="/mention">Mention</router-link> |
-    <router-link :to="{name: 'Caracteristique'}">Caracteristique</router-link> |
-    <router-link to="/niveau">Niveau</router-link> |
-    <router-link to="/semestre">Semestre</router-link> |
-    <router-link :to="{name: 'Composante'}">Composante</router-link> |
-    <router-link :to="{name: 'Localisation'}">Localisation</router-link> |
-    <router-link :to="{name: 'Recrutement'}">Modalite Recrutement</router-link> |
-    <router-link :to="{name: 'Pedagogique'}">Modalite Pedagogique</router-link> |
-    <router-link :to="{name: 'Partenaire'}">Partenaire</router-link> |
-    <router-link :to="{name: 'FormationModal'}">Modalite Formation</router-link> |
-    <router-link :to="{name: 'User'}">Utilisateur</router-link> |
-    <router-link to="/ue">UE</router-link>
-  </div>
+  <v-header role="banner"></v-header>
   <router-view/>
 </template>
 
-<style>
 
-#nav {
-  padding: 30px;
-}
+<script>
+  import VHeader from '../src/components/Header'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  export default {
+    components: {
+      VHeader
+    }
+  }
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+
+<style lang="postcss">
+  .header {
+    @apply flex justify-between;
+    color: white;
+    background: linear-gradient(91.71deg, #DD0E40 19.89%, #2B2463 108.53%);
+
+  }
+  .container-logo {
+    @apply flex my-auto mx-6;
+  }
+  .text-logo {
+    @apply font-bold ml-1 pl-0.5;
+    font-size: 18px;
+    line-height: 24px;
+    border-left: solid 2px white;
+    letter-spacing: 0.135em;
+  }
+  .dropdown-fade-enter-active,
+  .dropdown-fade-leave-active {
+    transition: all .5s ease-in-out;
+  }
+  .dropdown-fade-enter,
+  .dropdown-fade-leave-to {
+    opacity: 0;
+    transform: translateY(-12px);
+  }
+  .dropbtn {
+    @apply flex items-center justify-center mb-auto mx-auto h-16;
+    width: 200px;
+    padding: 0px;
+  }
+  .nav {
+    font-size: 16px;
+    line-height: 26px;
+    min-width: 50rem;
+  }
+  .nav div {
+    @apply pr-4;
+  }
+  .nav>div:not(:last-child):hover {
+    @apply font-bold;
+    background: rgba(255, 255, 255, 0.4);
+  }
+  .nav svg {
+    @apply mr-2;
+  }
+  .dropdown-content {
+    @apply max-w-sm flex flex-col;
+    display: none;
+    position: absolute;
+    background-color: #fff;
+    min-width: 300px;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    z-index: 1;
+  }
+  .dropdown-content a {
+    @apply text-sm font-medium;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+  }
+  .dropdown-content a:hover {
+    background-color: whitesmoke;
+  }
+  .nav>div:not(:last-child):hover .dropdown-content {
+    @apply flex flex-col;
+  }
+  [aria-hidden="true"] {
+  //display: none;
+  }
+  /* button {
+      background-color: transparent;
+      border: none;
+      cursor: pointer;
+      font-size: 1em;
+  }
+  button {
+      outline: none;
+  }
+  svg {
+      @apply mr-2;
+  }*/
+  #nav {
+    padding: 30px;
+  }
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
 </style>

82e875ab74ae484f00bdb3688964542af3de7bc0
Thibault <thibault.pelinski10@gmail.com>
2021-05-07 11:16:02 +0200

    fix

--- mcc/src/components/form/CompetenceForm.vue
+++ mcc/src/components/form/CompetenceForm.vue
@@ -1,53 +1,37 @@
 <template>
-    <div class="body h-screen">
     <form class="container w-auto">
         <div class="my-12 md:mx-6 sm:mx-6 xl:mx-56 lg:mx-56">
-        <h1>Ajouter une compétence</h1>
+            <h1>Ajouter une compétence</h1>
             <div class="flex flex-wrap">
                 <div class="w-full px-3 mb-6 md:mb-4">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                        Blocs
-                    </label>
-                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
-                           type="text"
-                           v-model="form.blocs" required
-                    >
+                            Blocs
+                        </label>
+                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2" type="text" v-model="form.blocs" required>
                 </div>
                 <div class="w-full px-3 mb-6 md:mb-4">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                        Seuil Bloc
-                    </label>
-                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
-                           type="text"
-                           v-model="form.seuilBlocs" required
-                    >
+                            Seuil Bloc
+                        </label>
+                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2" type="text" v-model="form.seuilBlocs" required>
                 </div>
                 <div class="w-full px-3 mb-6 md:mb-4">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                        ECTS
-                    </label>
-                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
-                           type="text"
-                           v-model="form.ECT" required
-                    >
+                            ECTS
+                        </label>
+                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2" type="text" v-model="form.ECT" required>
                 </div>
                 <div class="w-full px-3 mb-6 md:mb-4">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                        Coefficient
-                    </label>
-                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
-                           type="text"
-                           v-model="form.COEFF" required
-                    >
+                            Coefficient
+                        </label>
+                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2" type="text" v-model="form.COEFF" required>
                 </div>
                 <div class="w-full px-3 mb-6 md:mb-4">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                        Seuil
-                    </label>
-                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2"
-                           type="text"
-                           v-model="form.seuil" required
-                    >
+                            Seuil
+                        </label>
+                    <input class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 p-2" type="text" v-model="form.seuil" required>
                 </div>
                 <div class="w-full px-3 mt-12">
                     <button class="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded font-semibold text-sm" type="button" v-on:click="submit()">AJOUTER</button>
@@ -55,16 +39,11 @@
             </div>
         </div>
     </form>
-    </div>
 </template>
 
 <script>
-    
     import http from "../../http-common"
-    
-export default {
-
-
+    export default {
         name: "CompetenceForm",
         data() {
             return {
@@ -78,37 +57,30 @@ export default {
                 }
             }
         },
-        // methods: {
-        //     submit: function () {
-        //         http
-        //             .post( 'competences', this.form)
-
-        //         this.$router.push({ name: 'Competence'})
-        //     }
-        // }
 
         methods: {
-            submit: function () {
+            submit: function() {
                 http
-                    .post( 'competences', this.form)
-                    .then(function( response ){
+                    .post('competences', this.form)
+                    .then(function(response) {
                         this.stat = response.status
                         if (this.stat === 201) {
                             this.$toast.success(`Competence créée avec succès`, {
                                 position: "top-right"
                             })
                             setTimeout(this.$toast.clear, 3500)
-                            this.$router.push({ name: 'Competence' })
+                            this.$router.push({
+                                name: 'Competence'
+                            })
                         }
                     }.bind(this))
-                    .catch(function (error) {
+                    .catch(function(error) {
                         this.err = error.response.status
                         if (this.err === 400) {
                             this.$toast.error(`Champ invalide`, {
                                 position: "top-right"
                             })
-                        }
-                        else if (this.err === 422) {
+                        } else if (this.err === 422) {
                             this.$toast.error(`Entité impossible à traiter`, {
                                 position: "top-right"
                             })
